b449b6b82225183f05029ca2afef4cc7576c3007
oliburt <oliburt@gmail.com>
2019-10-17 13:35:21 +0100

    added padded to semantic Grid

--- space-invader-frontend/src/containers/logged_in/UserPageContainer.js
+++ space-invader-frontend/src/containers/logged_in/UserPageContainer.js
@@ -1,26 +1,24 @@
-import React, { Component } from 'react';
-import DashboardContainer from './DashboardContainer';
-import ChatContainer from './ChatContainer';
-import {Grid} from 'semantic-ui-react'
+import React, { Component } from "react";
+import DashboardContainer from "./DashboardContainer";
+import ChatContainer from "./ChatContainer";
+import { Grid } from "semantic-ui-react";
 
 export class UserPageContainer extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Welcome</h1>
-                  <Grid>
-    <Grid.Column width={10}>
-    <DashboardContainer />
-    </Grid.Column>
-    <Grid.Column width={6}>
-    <ChatContainer />
-    </Grid.Column>
-  </Grid>
-
-          
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h1>Welcome</h1>
+        <Grid padded>
+          <Grid.Column width={10}>
+            <DashboardContainer />
+          </Grid.Column>
+          <Grid.Column width={6}>
+            <ChatContainer />
+          </Grid.Column>
+        </Grid>
+      </div>
+    );
+  }
 }
 
 export default UserPageContainer;
