d326c9dd1a79b36e6e25ae1ba117f2ce017f22d0
oliburt <oliburt@gmail.com>
2019-10-18 11:50:48 +0100

    prettified some elements

--- space-invader-frontend/src/containers/logged_in/DashboardContainer.js
+++ space-invader-frontend/src/containers/logged_in/DashboardContainer.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import APODContainer from './APODContainer';
-import CountdownContainer from './CountdownContainer';
-import AsteroidContainer from './AsteroidContainer';
-import {Grid} from 'semantic-ui-react'
+import React from "react";
+import APODContainer from "./APODContainer";
+import CountdownContainer from "./CountdownContainer";
+import AsteroidContainer from "./AsteroidContainer";
+import { Grid } from "semantic-ui-react";
 
 const DashboardContainer = () => {
-    return (
-        <div>
-            <Grid>
-                <Grid.Column width={8}>
-            <APODContainer />
-            </Grid.Column>
-            <Grid.Column width={8}>
-            <CountdownContainer />
-            <AsteroidContainer />
-            </Grid.Column>
-            </Grid>
-        </div>
-    );
-}
+  return (
+    <div>
+      <Grid>
+        <Grid.Column width={8}>
+          <APODContainer />
+        </Grid.Column>
+        <Grid.Column width={8}>
+          <CountdownContainer />
+          <AsteroidContainer />
+        </Grid.Column>
+      </Grid>
+    </div>
+  );
+};
 
 export default DashboardContainer;
