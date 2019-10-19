aea5b3d3f3c2346b984535d674eb7d7e182caf77
oliburt <oliburt@gmail.com>
2019-10-19 16:09:29 +0100

    added actioncable and functions to handling received messages

--- space-invader-frontend/src/containers/logged_in/ChatContainer.js
+++ space-invader-frontend/src/containers/logged_in/ChatContainer.js
@@ -5,6 +5,8 @@ import MessageForm from "../../components/MessageForm";
 import { Grid } from "semantic-ui-react";
 import Adapter from '../../adapters/BackendAdapter';
 import '../../Chat.css'
+import { ActionCable } from "react-actioncable-provider";
+import Cable from "../../components/Cable";
 
 const ChatContainer = ({user}) => {
 
@@ -15,18 +17,46 @@ const ChatContainer = ({user}) => {
         Adapter.get(Adapter.CHATROOMS_URL).then(setChatrooms)
     }, [])
 
-    const getChannelNames = chatrooms => chatrooms.map(room => room.name)
+    const getChannelNames = chatrooms => chatrooms.map(room => ({id: room.id, name: room.name}))
 
-    const getSelectedChannel = (chatrooms, selectedChannel) => chatrooms.find(room => room.name === selectedChannel)
+    const getSelectedChannel = (chatrooms, selectedChannel) => chatrooms.find(room => room.id === selectedChannel)
+
+    const handleReceivedChatroom = response => {
+        console.log(response)
+        const { chatroom } = response;
+        setChatrooms([...chatrooms, chatroom]);
+    };
+
+    const handleReceivedMessage = response => {
+        const {message} = response
+        const chatroomsCopy = [...chatrooms]
+        const chatroom = chatroomsCopy.find(room => room.id === message.chatroom_id)
+        chatroom.messages = [...chatroom.messages, message]
+        setChatrooms(chatroomsCopy)
+    }
 
     return (
         
         <Grid>
+            <ActionCable 
+                channel={{ channel: 'ChatroomsChannel'}}
+                onReceived={handleReceivedChatroom}
+            />
+            {
+                chatrooms.length ? 
+                <Cable
+                    chatrooms={chatrooms}
+                    handleReceivedMessage={handleReceivedMessage}
+                /> :
+                null
+            }
+
+
             <Grid.Column className="noPadding" width={5}>
                 <Channels channels={getChannelNames(chatrooms)} handleClick={setSelectedChannel}/>
             </Grid.Column>
             <Grid.Column width={11}>
-                <Conversation conversation={getSelectedChannel(chatrooms, selectedChannel)} user={user}/>
+                <Conversation conversation={getSelectedChannel(chatrooms, selectedChannel)} currentUser={user}/>
                 {selectedChannel ? <MessageForm selectedChannel={getSelectedChannel(chatrooms, selectedChannel)} user={user} /> : null }
             </Grid.Column>
         </Grid>
