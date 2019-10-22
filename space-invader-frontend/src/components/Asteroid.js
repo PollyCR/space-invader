6ac9a53a149911e947fac53816fa55a48cbe1496
PollyCR <polly.c.reynolds@gmail.com>
2019-10-22 17:52:48 +0100

    funky arrow and all that jazz

--- space-invader-frontend/src/components/Asteroid.js
+++ space-invader-frontend/src/components/Asteroid.js
@@ -7,9 +7,9 @@ const Asteroid = props => {
     return (
         <Grid padded className = "asteroid-diagram">
           <Grid.Row>{props.asteroid.name} is currently {props.asteroid.close_approach_data[0].miss_distance.kilometers.split(".")[0]} kilometres from Earth.</Grid.Row>
-          <Grid.Column width = {6}><Image className = "asteroid-diagram" src = {require("../images/asteroid.png")}/></Grid.Column>
-          <Grid.Column className = "asteroid-distance" width = {2}></Grid.Column>
-          <Grid.Column width = {8}><Image src = {require("../images/earth.png")}/></Grid.Column>
+          <Grid.Column width = {4}><Image className = "asteroid-image" src = {require("../images/asteroid.png")}/></Grid.Column>
+          <Grid.Column className = "asteroid-distance" width = {6}><Image className = "asteroid-diagram" src = {require("../images/arrow.gif")}/></Grid.Column>
+          <Grid.Column width = {6}><Image src = {require("../images/earth.png")}/></Grid.Column>
         </Grid>
     );
   } else {
