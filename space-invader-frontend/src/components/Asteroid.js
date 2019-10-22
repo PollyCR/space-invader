38e4c20265cd251eaf5a980c4ae3c13624fc1705
PollyCR <polly.c.reynolds@gmail.com>
2019-10-22 16:47:28 +0100

    changed asteroid layout

--- space-invader-frontend/src/components/Asteroid.js
+++ space-invader-frontend/src/components/Asteroid.js
@@ -6,10 +6,10 @@ const Asteroid = props => {
   if (props.asteroid) {
     return (
         <Grid padded className = "asteroid-diagram">
-          <Grid.Row>{props.asteroid.name}</Grid.Row>
-          <Grid.Column width = {4}><Image className = "asteroid-diagram" src = {require("../images/asteroid.png")}/></Grid.Column>
-          <Grid.Column className = "asteroid-distance" width = {6}>{props.asteroid.name} is currently {props.asteroid.close_approach_data[0].miss_distance.kilometers.split(".")[0]} kilometres from Earth.</Grid.Column>
-          <Grid.Column width = {6}><Image src = {require("../images/earth.png")}/></Grid.Column>
+          <Grid.Row>{props.asteroid.name} is currently {props.asteroid.close_approach_data[0].miss_distance.kilometers.split(".")[0]} kilometres from Earth.</Grid.Row>
+          <Grid.Column width = {6}><Image className = "asteroid-diagram" src = {require("../images/asteroid.png")}/></Grid.Column>
+          <Grid.Column className = "asteroid-distance" width = {2}></Grid.Column>
+          <Grid.Column width = {8}><Image src = {require("../images/earth.png")}/></Grid.Column>
         </Grid>
     );
   } else {
